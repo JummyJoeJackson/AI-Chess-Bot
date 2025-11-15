6f259a3b7b4115967f384b71a80d37682bd1a1b8
Diego Gonzalez <116301465+JummyJoeJackson@users.noreply.github.com>
2025-11-15 18:20:33 -0500

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,41 +1,41 @@
 from .utils import *
 
 
-# Set repo_id for model loading
-repo_id = "JummyJoeJackson/chess-bot-model"
-
+MODEL_GDRIVE_ID = ""
+MODEL_LOCAL_PATH = "./Training/model_weights.pt"
+config = ChessBotConfig()
 
 # Entrypoint function called to get the next move
 @chess_manager.entrypoint
 def get_move(ctx: GameContext) -> str:
     board = ctx.board
     
-    """
     # Get or create cached model
-    ctx = chess_manager.context
+    ctx = chess_manager._ctx
     model = ctx.state.get('model')
+    config = ChessBotConfig()
     if model is None:
         try:
-            model = load_model_from_hf()
+            model = load_model(config)
         except Exception as e:
-            print("Failed to load model from HF Hub, initializing fresh model", e)
-            model = EvalNet()
+            print("Failed to load model, initializing fresh model", e)
+            model = EvalNet(config)
         ctx.state['model'] = model
     
-   
+    # Determine best move using minimax with the model
     try:
         best_move = make_best_move(board, model, depth=3)
     except Exception as e:
         print("Error during move calculation:", e)
-    
-    """
-
-    # Fallback: make a random legal move
-    best_move = list(board.legal_moves)[0]
+        # Fallback: make a random legal move
+        best_move = list(board.legal_moves)[0]
 
     # Ensure best_move is not None
-    if not best_move:
-        raise ValueError("No legal moves available")
+    if not best_move or best_move not in board.legal_moves:
+        print(f"invalid move generated: {best_move} falling back to random move")
+        best_move = next(iter(board.legal_moves), None)
+        if best_move is None:
+            raise ValueError("No legal moves available")
     return best_move
 
 
@@ -43,8 +43,10 @@ def get_move(ctx: GameContext) -> str:
 @chess_manager.reset
 def reset_func(ctx: GameContext):
     try:
-        model = load_model_from_hf()
+        model = load_model(config)
     except Exception as e:
-        print("Failed to load model from HF Hub in reset, initializing fresh model", e)
-        model = EvalNet()
+        print("Failed to load model from in reset, initializing fresh model", e)
+        model = EvalNet(config)
+        torch.save(model.state_dict(), "model_weights.pt")
+        print("Saved fresh fallback model weights as model_weights.pt")
     ctx.state['model'] = model
